c09d9ef76704bbb98f57549f6d82e4325ba1f0d0
Villager Z <git.villagerz@pm.me>
2020-05-23 19:09:54 -0400

    added Aeson library and User data constructor

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,8 +1,27 @@
 module Lib
-    ( someFunc
+    ( someFunc,
+      User (..)
     ) where
 
 import ClassyPrelude
+import Data.Aeson
 
+data User = User { userId :: Int
+                 , userName :: Text
+                 , userHobbies :: [Text]
+                 } deriving (Show)
+
+instance ToJSON User where
+  toJSON (User id name hobs) = object [ "id" .= id
+                                      , "name" .= name
+                                      , "hobbies" .= hobs
+                                      ]
+
+instance FromJSON User where
+  parseJSON = withObject "User" $ \v ->
+    User <$> v .: "id"
+         <*> v .: "name"
+         <*> v .: "hobbies"
+         
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
