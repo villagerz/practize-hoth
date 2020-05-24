d05995a126351d4d08b114c0276b32f512db9663
Villager Z <git.villagerz@pm.me>
2020-05-24 12:26:24 -0400

    added Email and Password smart constructors to Data.Auth

--- src/Domain/Auth.hs
+++ src/Domain/Auth.hs
@@ -1,3 +1,29 @@
-module Domain.Auth where
+module Domain.Auth
+  ( mkEmail
+  , mkPassword
+  )
+where
+
+import ClassyPrelude
 
 data Auth = Auth
+  { authEmail :: Email
+  , authPassword :: Password
+  } deriving (Show, Eq)
+
+data RegistrationError = RegistrationErrorEmailTaken deriving (Show, Eq)
+
+newtype Email = Email { emailRaw :: Text } deriving (Show, Eq)
+newtype Password = Password { passwordRaw :: Text } deriving (Show, Eq)
+
+mkEmail :: Text -> Either [Text] Email
+mkEmail = error "TODO: not yet defined"
+
+rawEmail :: Email -> Text
+rawEmail = emailRaw
+
+mkPassword :: Text -> Either [Text] Password
+mkPassword = error "TODO: net yet implemented"
+
+rawPassword :: Password -> Text
+rawPassword = passwordRaw
