47f0b3ce9b4741804a91cee2988f4f3bd58033e6
Villager Z <git.villagerz@pm.me>
2020-05-31 17:01:29 -0400

    added a prrof of concept handle in Lib, someFunc that calls action, to see logging happening in repl. Needed slight deviation from the book for some reason.

--- src/Lib.hs
+++ src/Lib.hs
@@ -9,18 +9,22 @@ import Data.Aeson
 import Data.Aeson.TH
 import qualified Adapter.InMemory.Auth as M
 import Domain.Auth
+import Katip
+import qualified Control.Monad.Fail as Fail
 
 type State = TVar M.State
-newtype App a = App { unApp :: ReaderT State IO a } deriving (Applicative
-                                                              ,Functor
-                                                              , Monad
-                                                              , MonadReader State
-                                                              , MonadIO)
+newtype App a = App {
+  unApp :: ReaderT State (KatipContextT IO) a
+  } deriving (Applicative, Functor, Monad, MonadReader State, MonadIO, KatipContext, Katip)
+
 instance AuthRepo App where
   addAuth = M.addAuth
   setEmailAsVerified = M.setEmailAsVerified
   findUserByAuth = M.findUserByAuth
   findEmailFromUser = M.findEmailFromUser
+
+instance Fail.MonadFail App where
+  fail _ = error "this should not have happened"
 
 instance SessionRepo App where
   newSession = M.newSession
@@ -29,10 +33,15 @@ instance SessionRepo App where
 instance EmailVerificationNotif App where
   notifyEmailVerification = M.notifyEmailVerification
 
-run :: State -> App a -> IO a
-run s = flip runReaderT s . unApp
+run :: LogEnv -> State -> App a -> IO a
+run le s = runKatipContextT le () mempty . flip runReaderT s . unApp
 
-
+withKatip :: (LogEnv -> IO a) -> IO a
+withKatip app = bracket createLogEnv closeScribes app
+  where createLogEnv = do
+          le <- initLogEnv "HAuth" "prod"
+          stdoutScribe <- mkHandleScribe ColorIfTerminal stdout (permitItem InfoS)  V2
+          registerScribe "stdout" stdoutScribe defaultScribeSettings le
 
 data User = User { userId :: Int
                  , userName :: Text
@@ -45,4 +54,20 @@ $(let strcutName = nameBase ''User
    in deriveJSON options ''User)
 
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = withKatip $ \le -> do
+  state <- newTVarIO M.initialState
+  run le state action
+
+action :: App ()
+action = do
+      let e = either undefined id $ mkEmail "test@test.com"
+          pswd = either undefined id $ mkPassword "12HJbvv"
+          auth = Auth e pswd
+      register auth
+      Just v <- M.getNotificationsForEmail e -- I needed to implement an instance of Monaf.Fail for this to work
+      verifyEmail  v
+      Right session <- login auth
+      Just uid <- resolveSessionId session
+      Just registeredEmail <- getUser uid
+      return ()
+      
