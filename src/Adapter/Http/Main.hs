2ab9e5221db2238fba556398463168e645ea59f8
Villager Z <git.villagerz@pm.me>
2020-06-17 21:26:35 -0400

    CH07: added various scotty related dependencies. Wrestled with dependency hell with stack and libraries not in the LTS.

--- src/Adapter/Http/Main.hs
+++ src/Adapter/Http/Main.hs
@@ -6,5 +6,21 @@ import           Web.Scotty.Trans
 main ∷ IO ()
 main = scottyT 3000 id routes
 
+-- routes are processed in order till first hit
+-- hence notFound at end
 routes ∷ (MonadIO m) ⇒ ScottyT LText m ()
-routes = get "/hello" $ text "Hello!"
+routes = do
+  httpdo get "/" "home"
+  httpdo get "/hello" "Hello!"
+  get "/hello/:name" $ do
+    name ← param "name"
+    (text $ "Hello, " <> name) `rescue` \_ → text "you of no name"
+  httpdo post "/users" "adding user"
+  httpdo put "/users/:id" "updating user"
+  httpdo patch "/users/:id" "partially updating users"
+  httpdo delete "/users/:id" "deleting user"
+  httpdo matchAny "/admin" "I don't care about your Http verb"
+  httpdo options (regex ".*") "CORS usually use this"
+  notFound $ text "404"
+
+httpdo verb route text' = verb route $ text text'
